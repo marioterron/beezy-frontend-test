775523ccc7fa22afdf0350a26a9312cf033082c1
Mario Terron <marioterron157@gmail.com>
2018-01-18 16:34:49 +0100

    Refactored form component

--- src/components/BookstoreForm/BookstoreForm.js
+++ src/components/BookstoreForm/BookstoreForm.js
@@ -5,20 +5,9 @@ import './bookstore-form.scss';
 
 const FormItem = Form.Item;
 
-const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 14 }
-};
-
-const buttonItemLayout = {
-  wrapperCol: { span: 14, offset: 4 }
-};
-
-const formLayout = 'inline';
-
 const BookStoreForm = () => {
   return (
-    <Form layout={formLayout} className="bookstore-form">
+    <Form layout="inline" className="bookstore-form">
       <h1 className="bookstore-form__title">Add a new book to our awesome bookstore</h1>
       <FormItem label="Title">
         <Input />
@@ -26,7 +15,7 @@ const BookStoreForm = () => {
       <FormItem label="Author">
         <Input />
       </FormItem>
-      <FormItem label="Genre">
+      <FormItem label="Price">
         <Input />
       </FormItem>
       <FormItem>
