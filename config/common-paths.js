5967dad4f3771e30f73d88e009bc89b2b3b1e2a8
Mario Terron <marioterron157@gmail.com>
2018-01-19 13:56:48 +0100

    Updated configuration for production bundle

--- config/common-paths.js
+++ config/common-paths.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  root: path.resolve(__dirname, '../'),
   src: path.resolve(__dirname, '../', 'src'),
   build: path.resolve(__dirname, '../', 'build'),
   public: path.resolve(__dirname, '../', 'public')
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const commonPaths = require('./common-paths');
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
@@ -19,6 +20,18 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin([commonPaths.build], { root: commonPaths.root }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: `${commonPaths.public}/favicon.png`,
+        to: `${commonPaths.build}/favicon.png`,
+        toType: 'file'
+      }
+    ]),
     new ExtractTextWebpackPlugin('style.[contenthash].css'),
     new UglifyJsWebpackPlugin()
   ]
